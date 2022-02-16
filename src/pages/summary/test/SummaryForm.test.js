323687772cc8afbe42dba3ab0c31868504df2fd3
Anna Roos <anna.flytstrom@gmail.com>
2022-02-16 19:51:26 +0100

    installed user-event

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -1,27 +1,32 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
 describe('SummaryForm component', () => {
   test('checkbox is unchecked by default and button is disabled', () => {
     render(<SummaryForm />);
+
     const checkbox = screen.getByRole('checkbox', {
       name: /terms and conditions/i,
     });
     const button = screen.getByRole('button', { name: /confirm order/i });
+
     expect(checkbox).not.toBeChecked();
     expect(button).toBeDisabled();
   });
 
-  test('checking the checkbox enables the button and unchecking disables it', () => {
+  test('checking the checkbox enables the button and unchecking disables it', async () => {
     render(<SummaryForm />);
+
     const checkbox = screen.getByRole('checkbox', {
       name: /terms and conditions/i,
     });
     const button = screen.getByRole('button', { name: /confirm order/i });
-    fireEvent.click(checkbox);
+    const user = userEvent.setup();
+    await user.click(checkbox);
     expect(button).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(button).toBeDisabled();
   });
 });
