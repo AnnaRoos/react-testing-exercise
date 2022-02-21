69a23a74690b6bf8131a1e6b004740a1aa57e28f
Anna Roos <anna.flytstrom@gmail.com>
2022-02-21 11:44:26 +0100

    fixed error in test by moving it into async test

--- src/pages/entry/ToppingOption.js
+++ src/pages/entry/ToppingOption.js
@@ -1,11 +1,11 @@
 import React from 'react';
+
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 
 const ToppingOption = ({ imagePath, name, updateItemCount }) => {
   const handleChange = (event) => {
-    const oneOrZero = event.target.checked ? 1 : 0;
-    updateItemCount(name, oneOrZero);
+    updateItemCount(name, event.target.checked ? 1 : 0);
   };
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
@@ -14,7 +14,7 @@ const ToppingOption = ({ imagePath, name, updateItemCount }) => {
         alt={`${name} topping`}
         src={`http://localhost:3030/${imagePath}`}
       />
-      <Form.Group controlId={`${name}-count`}>
+      <Form.Group controlId={`${name}-checkbox`}>
         <Form.Check type="checkbox" onChange={handleChange} label={name} />
       </Form.Group>
     </Col>
--- src/pages/entry/tests/totalUpdates.test.js
+++ src/pages/entry/tests/totalUpdates.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '../../../test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
+import OrderEntry from '../OrderEntry';
 
-describe('Check total updates', () => {
+describe('subtotals', () => {
   test('scoops subtotal should update when scoops change', async () => {
     render(<Options optionType="scoops" />);
 
@@ -66,5 +67,105 @@ describe('Check total updates', () => {
     await user.click(strawberryCheckbox);
 
     expect(toppingsSubtotals).toHaveTextContent('1.50');
+  });
+});
+
+describe('grand total', () => {
+  test('grand total should start at 0.00update properly when scoops are added first', async () => {
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByRole('heading', {
+      name: /grand total: \$/i,
+    });
+
+    expect(grandTotal).toHaveTextContent('0.00');
+
+    const chocolateInput = await screen.findByRole('spinbutton', {
+      name: 'Chocolate',
+    });
+
+    const user = userEvent.setup();
+    await user.clear(chocolateInput);
+    await user.type(chocolateInput, '2');
+
+    expect(grandTotal).toHaveTextContent('4.00');
+
+    const mAndMsCheckbox = await screen.findByRole('checkbox', {
+      name: 'M&Ms',
+    });
+
+    await user.click(mAndMsCheckbox);
+
+    expect(grandTotal).toHaveTextContent('5.50');
+  });
+
+  test('grand total should update properly when toppings are added first', async () => {
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByRole('heading', {
+      name: /grand total: \$/i,
+    });
+
+    const fudgeCheckbox = await screen.findByRole('checkbox', {
+      name: 'Fudge',
+    });
+
+    const user = userEvent.setup();
+    await user.click(fudgeCheckbox);
+
+    expect(grandTotal).toHaveTextContent('1.50');
+
+    const vanillaInput = await screen.findByRole('spinbutton', {
+      name: 'Vanilla',
+    });
+
+    await user.clear(vanillaInput);
+    await user.type(vanillaInput, '3');
+
+    expect(grandTotal).toHaveTextContent('7.50');
+  });
+
+  test('grand total should update properly when items are removed', async () => {
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByRole('heading', {
+      name: /grand total: \$/i,
+    });
+    const chocolateInput = await screen.findByRole('spinbutton', {
+      name: 'Chocolate',
+    });
+
+    const user = userEvent.setup();
+
+    await user.clear(chocolateInput);
+    await user.type(chocolateInput, '4');
+
+    const strawberryCheckbox = await screen.findByRole('checkbox', {
+      name: 'Strawberry',
+    });
+    const fudgeCheckbox = await screen.findByRole('checkbox', {
+      name: 'Fudge',
+    });
+
+    await user.click(strawberryCheckbox);
+    await user.click(fudgeCheckbox);
+
+    expect(grandTotal).toHaveTextContent('11.00');
+
+    await user.type(chocolateInput, '2');
+
+    expect(grandTotal).toHaveTextContent('7.00');
+
+    await user.click(strawberryCheckbox);
+
+    expect(grandTotal).toHaveTextContent('5.50');
+
+    await user.click(fudgeCheckbox);
+
+    expect(grandTotal).toHaveTextContent('4.00');
+
+    await user.type(chocolateInput, '0');
+
+    expect(grandTotal).toHaveTextContent('0.00');
   });
 });
